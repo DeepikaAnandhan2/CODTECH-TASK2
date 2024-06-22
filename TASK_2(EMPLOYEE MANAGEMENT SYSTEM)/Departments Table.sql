55bddf408b89f8db04fbc94740b620345eb65c8d
DEEPIKA A <145033159+DeepikaAnandhan2@users.noreply.github.com>
2024-06-22 10:29:37 +0530

    Delete TASK_2(EMPLOYEE MANAGEMENT SYSTEM) directory

--- TASK_2(EMPLOYEE MANAGEMENT SYSTEM)/Departments Table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Departments (
-    DepartmentID INT PRIMARY KEY AUTO_INCREMENT,
-    DepartmentName VARCHAR(100) NOT NULL,
-    Location VARCHAR(100)
-);
--- TASK_2(EMPLOYEE MANAGEMENT SYSTEM)/JobPosition Table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE JobPositions (
-    PositionID INT PRIMARY KEY AUTO_INCREMENT,
-    PositionName VARCHAR(100) NOT NULL,
-    DepartmentID INT,
-    FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
-);
